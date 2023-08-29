9856f67ba887b197757bb67b52402612e3cd7c96
Kyrre Havik <kyrremann@gmail.com>
2023-08-29 10:23:22 +0200

    feat: større refaktorering

--- main.go
+++ main.go
@@ -2,168 +2,25 @@ package main
 
 import (
 	"context"
-	"flag"
-	"fmt"
-	"io/ioutil"
 	"net/http"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
 
-	"cloud.google.com/go/storage"
-	"github.com/go-chi/chi"
-	"github.com/sirupsen/logrus"
-	"google.golang.org/api/iterator"
+	"github.com/navikt/nada-bucket-proxy/pkg/api"
 )
 
-type Config struct {
-	BucketName string
-}
-
-type API struct {
-	gcsClient  *storage.Client
-	bucketName string
-	router     *chi.Mux
-	log        *logrus.Entry
-	quartoUUID string
-	quartoPath string
-}
-
-func NewAPI(ctx context.Context, bucketName string, log *logrus.Entry) (*API, error) {
-	gcsClient, err := storage.NewClient(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	quartoUUID := os.Getenv("QUARTO_UUID")
-	if quartoUUID == "" {
-		return nil, fmt.Errorf("no quarto UUID configured %v", quartoUUID)
-	}
-
-	quartoPath := os.Getenv("QUARTO_PATH")
-	if quartoPath == "" {
-		return nil, fmt.Errorf("no quarto path configured %v", quartoPath)
-	}
-
-	router := chi.NewRouter()
-	api := &API{
-		gcsClient:  gcsClient,
-		bucketName: bucketName,
-		router:     router,
-		log:        log,
-		quartoUUID: quartoUUID,
-		quartoPath: quartoPath,
-	}
-	api.setupRoutes(router)
-
-	return api, nil
-}
-
-func (a *API) setupRoutes(router *chi.Mux) {
-	router.Route("/"+a.quartoPath, func(r chi.Router) {
-		r.Use(a.QuartoMiddleware)
-		r.Get("/*", a.GetQuarto)
-	})
-}
-
 func main() {
-	cfg := Config{}
-	flag.StringVar(&cfg.BucketName, "bucket", os.Getenv("GCS_QUARTO_BUCKET"), "The quarto storage bucket")
 	ctx := context.Background()
-	log := logrus.New()
 
-	api, err := NewAPI(ctx, cfg.BucketName, log.WithField("subsystem", "api"))
+	router, err := api.NewRouter(ctx)
 	if err != nil {
 		panic(err)
 	}
 
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: api.router,
+		Handler: router,
 	}
 
 	if err := server.ListenAndServe(); err != nil {
 		panic(err)
-	}
-}
-
-func (a *API) QuartoMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		regex, _ := regexp.Compile(`[\n]*\.[\n]*`) // check if object path has file extension
-		if !regex.MatchString(r.URL.Path) {
-			a.Redirect(w, r)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (a *API) Redirect(w http.ResponseWriter, r *http.Request) {
-	objs := a.gcsClient.Bucket(a.bucketName).Objects(r.Context(), &storage.Query{Prefix: a.quartoUUID + "/"})
-	objPath, err := a.findIndexPage(a.quartoUUID, objs)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
-	r.URL.Path = "/"
-	http.Redirect(w, r, objPath, http.StatusSeeOther)
-}
-
-func (a *API) GetQuarto(w http.ResponseWriter, r *http.Request) {
-	path := a.quartoUUID + "/" + strings.TrimPrefix(r.URL.Path, fmt.Sprintf("/%v/", a.quartoPath))
-	attr, objBytes, err := a.GetObject(r.Context(), path)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Add("content-type", attr.ContentType)
-	w.Header().Add("content-length", strconv.Itoa(int(attr.Size)))
-	w.Header().Add("content-encoding", attr.ContentEncoding)
-
-	w.Write(objBytes)
-}
-
-func (a *API) GetObject(ctx context.Context, path string) (*storage.ObjectAttrs, []byte, error) {
-	obj := a.gcsClient.Bucket(a.bucketName).Object(path)
-	reader, err := obj.NewReader(ctx)
-	if err != nil {
-		return nil, []byte{}, err
-	}
-
-	datab, err := ioutil.ReadAll(reader)
-	if err != nil {
-		return nil, []byte{}, err
-	}
-
-	attr, err := obj.Attrs(ctx)
-	if err != nil {
-		return nil, []byte{}, err
-	}
-
-	return attr, datab, nil
-}
-
-func (a *API) findIndexPage(qID string, objs *storage.ObjectIterator) (string, error) {
-	page := ""
-	for {
-		o, err := objs.Next()
-		if err == iterator.Done {
-			if page == "" {
-				return "", fmt.Errorf("could not find html for id %v", qID)
-			}
-			return page, nil
-		}
-		if err != nil {
-			a.log.WithError(err).Error("searching for index page in bucket")
-			return "", fmt.Errorf("index page not found")
-		}
-
-		if strings.HasSuffix(strings.ToLower(o.Name), "/index.html") {
-			return a.quartoPath + "/" + strings.TrimPrefix(o.Name, a.quartoUUID+"/"), nil
-		} else if strings.HasSuffix(strings.ToLower(o.Name), ".html") {
-			page = a.quartoPath + "/" + strings.TrimPrefix(o.Name, a.quartoUUID+"/")
-		}
 	}
 }
---
+++ pkg/api/api.go
@@ -0,0 +1,146 @@
+package api
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"cloud.google.com/go/storage"
+	"github.com/go-chi/chi"
+	"google.golang.org/api/iterator"
+)
+
+type API struct {
+	gcsClient  *storage.Client
+	bucketName string
+	quartoUUID string
+	quartoPath string
+}
+
+func NewRouter(ctx context.Context) (*chi.Mux, error) {
+	gcsClient, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	bucketName := os.Getenv("GCS_QUARTO_BUCKET")
+	if bucketName == "" {
+		return nil, fmt.Errorf("no bucket name configured")
+	}
+
+	quartoUUID := os.Getenv("QUARTO_UUID")
+	if quartoUUID == "" {
+		return nil, fmt.Errorf("no quarto UUID configured")
+	}
+
+	quartoPath := os.Getenv("QUARTO_PATH")
+	if quartoPath == "" {
+		return nil, fmt.Errorf("no quarto path configured")
+	}
+
+	router := chi.NewRouter()
+	api := &API{
+		gcsClient:  gcsClient,
+		bucketName: bucketName,
+		quartoUUID: quartoUUID,
+		quartoPath: quartoPath,
+	}
+	api.setupRoutes(router)
+
+	return router, nil
+}
+
+func (a *API) setupRoutes(router *chi.Mux) {
+	router.Route("/"+a.quartoPath, func(r chi.Router) {
+		r.Use(a.quartoMiddleware)
+		r.Get("/*", a.GetQuarto)
+	})
+}
+
+func (a *API) quartoMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		regex, _ := regexp.Compile(`[\n]*\.[\n]*`) // check if object path has file extension
+		if !regex.MatchString(r.URL.Path) {
+			a.redirect(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (a *API) redirect(w http.ResponseWriter, r *http.Request) {
+	objects := a.gcsClient.Bucket(a.bucketName).Objects(r.Context(), &storage.Query{Prefix: a.quartoUUID + "/"})
+	objPath, err := a.findIndexPage(objects)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	r.URL.Path = "/"
+	http.Redirect(w, r, objPath, http.StatusSeeOther)
+}
+
+func (a *API) GetQuarto(w http.ResponseWriter, r *http.Request) {
+	path := a.quartoUUID + "/" + strings.TrimPrefix(r.URL.Path, fmt.Sprintf("/%v/", a.quartoPath))
+	attr, objBytes, err := a.GetObject(r.Context(), path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Add("content-type", attr.ContentType)
+	w.Header().Add("content-length", strconv.Itoa(int(attr.Size)))
+	w.Header().Add("content-encoding", attr.ContentEncoding)
+
+	w.Write(objBytes)
+}
+
+func (a *API) GetObject(ctx context.Context, path string) (*storage.ObjectAttrs, []byte, error) {
+	obj := a.gcsClient.Bucket(a.bucketName).Object(path)
+	reader, err := obj.NewReader(ctx)
+	if err != nil {
+		return nil, []byte{}, err
+	}
+
+	datab, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, []byte{}, err
+	}
+
+	attr, err := obj.Attrs(ctx)
+	if err != nil {
+		return nil, []byte{}, err
+	}
+
+	return attr, datab, nil
+}
+
+func (a *API) findIndexPage(objects *storage.ObjectIterator) (string, error) {
+	page := ""
+	for {
+		object, err := objects.Next()
+		if err == iterator.Done {
+			if page == "" {
+				return "", fmt.Errorf("could not find html for quarto")
+			}
+
+			return page, nil
+		}
+
+		if err != nil {
+			return "", fmt.Errorf("index page not found")
+		}
+
+		if strings.HasSuffix(strings.ToLower(object.Name), "/index.html") {
+			return a.quartoPath + "/" + strings.TrimPrefix(object.Name, a.quartoUUID+"/"), nil
+		} else if strings.HasSuffix(strings.ToLower(object.Name), ".html") {
+			page = a.quartoPath + "/" + strings.TrimPrefix(object.Name, a.quartoUUID+"/")
+		}
+	}
+}
